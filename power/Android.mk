sanshu-rom/android_device_honor_frd

update power hal

power/Android.mk
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_SRC_FILES := power_frd.c
+LOCAL_SRC_FILES := power_frd.c utils.c power-feature-default.c
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional

OpenKirin/android_device_huawei_eva

Change-Id: I9022f716e48326bf02f720dfe09e5c060dd3aa49

power/Android.mk
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_SRC_FILES := power_eva.c
+LOCAL_SRC_FILES := power_eva.c utils.c power-feature-default.c
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional